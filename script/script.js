00ae6d2dbb1a77e29f79fd3960a47727b80f1790
Catur Setyono <catursetyono542@gmail.com>
2025-12-27 21:03:51 +0700

    Initialize data_project array with project details and render to the DOM

--- script/script.js
+++ script/script.js
@@ -1,1 +1,34 @@
-const data_project =[];
+const data_project = [
+  {
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4dHVLp6Jdx3sD8i0qIFgWeJM0Ic12XkAozA&s",
+    title: "Meta Verse",
+    desc: "This is about meta verse project and its uses. using VR and AR technology.",
+  },
+  {
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4dHVLp6Jdx3sD8i0qIFgWeJM0Ic12XkAozA&s",
+    title: "Meta Verse",
+    desc: "This is about meta verse project and its uses.",
+  },
+  {
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4dHVLp6Jdx3sD8i0qIFgWeJM0Ic12XkAozA&s",
+    title: "Meta Verse",
+    desc: "This is about meta verse project and its uses.",
+  },
+  {
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4dHVLp6Jdx3sD8i0qIFgWeJM0Ic12XkAozA&s",
+    title: "Meta Verse",
+    desc: "This is about meta verse project and its uses. ",
+  },
+];
+
+const projectContainer = document.getElementsByClassName("container")[0];
+data_project.forEach((project) => {
+  const projectElement = document.createElement("div");
+  projectElement.classList.add("card");
+  projectElement.innerHTML = `
+  <img src="${project.url}" alt="${project.title}" />
+    <h3>${project.title}</h3>
+    <p>${project.desc}</p>
+  `;
+  projectContainer.appendChild(projectElement);
+});
